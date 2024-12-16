1760838679ff3a325d03e01f2ed342b8bf35776a
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2024-12-16 11:01:04 +0200

    Before Making folders&files

--- data_loader.py
+++ data_loader.py
@@ -2,37 +2,48 @@ import os
 import cv2
 
 class DataLoader:
-    def __init__(self, train_path='./dataset/train/', val_path='./dataset/val/'):
-        '''Loads training and validation data supposing it's organized as follows:
-        The train_path and val_path should have 3 folders (images, masks, detections)
-        All of the 3 folders should have subfolders one for each patient.
-        Images and masks should be corresponding to each other and named the same.
-        Detections of each patient should include text files named with the scan number
-        and each scan files includes a line or more of 4 numbers each (xMin, yMin, xMax, yMax)'''
+    def __init__(self, train_path='dataset/train/', val_path='dataset/val/'):
         self.train_path = train_path
         self.val_path = val_path
+
+    def load_data(self):
+        '''Reads the training & validation data from the train_path & val_path.
+        Returns 2 dictionaries.
+        Each one has string keys as the scanID (ex: "Subject0_0).
+        And each value is a list of [Image, Mask, detections_list]
+        where detections list may include 0 or more lists
+        of 4 numbers eachlocating the bounding box'''
+        self.image_height, self.image_width = cv2.imread("dataset/train/images/Subject_0/0.png", cv2.IMREAD_UNCHANGED).shape
+        print(f"The dataset consists of {self.image_height} x {self.image_width} grayscale images")
         
-        print('[ Loading Training Data ]  ', end='')
-        self.train_data = self.load_data(self.train_path)
-        print('Done')
+        print('[ Loading Training Data   ]  ', end='')
+        train_data = self.__load(self.train_path)
+        train_cancer_num, train_healthy_num = self.__count_cancer(train_data)
+        print(f'Loaded [{len(train_data)}] Scans ({train_cancer_num} Cancer + {train_healthy_num} Healthy)')
+        
         print('[ Loading Validation Data ]  ', end='')
-        self.val_data = self.load_data(self.val_path)
-        print('Done')
-    
-    def load_data(self, data_path):
+        val_data = self.__load(self.val_path)
+        val_cancer_num,val_healthy_num = self.__count_cancer(val_data)
+        print(f'Loaded [{len(val_data)}] Scans ({val_cancer_num} Cancer + {val_healthy_num} Healthy)')
+        return train_data, val_data
+
+    def __load(self, data_path):
+        '''Loads the training / validation data using the given path'''
+        os.makedirs(f'preprocessed/images_and_detections/{data_path}/', exist_ok=True)
+        
         data = {}
         for patient in os.listdir(os.path.join(data_path, 'images/')):
+            os.makedirs(subject_output_folder, exist_ok=True)
             for image_name in os.listdir(os.path.join(data_path, 'images/', patient)):
                 image_path = os.path.join(data_path, 'images/', patient, image_name)
                 img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-                
+
                 scan_ID, extension = os.path.splitext(image_name)
                 scan_ID = patient + '_' + scan_ID
                 data[scan_ID] = [img]
 
             for mask_name in os.listdir(os.path.join(data_path, 'masks/', patient)):
                 mask_path = os.path.join(data_path, 'masks/', patient, mask_name)
-                
                 scan_ID, extension = os.path.splitext(mask_name)
                 scan_ID = patient + '_' + scan_ID
                 mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
@@ -40,21 +51,42 @@ class DataLoader:
 
             for detection_name in os.listdir(os.path.join(data_path, 'detections/', patient)):
                 detection_path = os.path.join(data_path, 'detections/', patient, detection_name)
-                detections = self.read_detections(detection_path)
+                detections = self.__read_detections(detection_path)
 
                 scan_ID, extension = os.path.splitext(detection_name)
                 scan_ID = patient + '_' + scan_ID
                 data[scan_ID].append(detections)
-        
+
         for scan in data.values():
             if len(scan) == 2:
-                scan.append(None)
+                scan.append([])
         return data
-    
-    def read_detections(self, path):
+
+    def __read_detections(self, path):
+        '''Reads cancer detections from a text file in path'''
         detections = []
+        if not os.path.exists(path):
+            print(f'Error: Reading a detection from a wrong path')
+            return ()
         with open(path, 'r') as file:
             for line in file:
-                coordinates = [int(num.strip()) for num in line.split(',')]
+                xmin, ymin, xmax, ymax = [int(num.strip()) for num in line.split(',')]
+                bbox_x_center = (xmin + xmax) / 2 / self.image_width
+                bbox_y_center = (ymin + ymax) / 2 / self.image_height
+                bbox_width = (xmax - xmin) / self.image_width
+                bbox_height = (ymax - ymin) / self.image_height
+                coordinates = [1, bbox_x_center, bbox_y_center, bbox_width, bbox_height]
                 detections.append(tuple(coordinates))
         return tuple(detections)
+
+    def __count_cancer(self, data):
+        '''Counts the number of scans that do/doesn't have
+        cancer detections in the dictionary "data"'''
+        cancer_sum = 0
+        healthy_sum = 0
+        for image, scan, detections in data.values():
+            if len(detections) == 0:
+                healthy_sum += 1
+            else:
+                cancer_sum += 1
+        return cancer_sum, healthy_sum
