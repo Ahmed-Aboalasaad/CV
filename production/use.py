cbce05c1993eb86cc3bdb919d835a1248bc5fb6a
Ahmed Aboalesaad <ahmed.aboalisaad@gmail.com>
2024-12-21 18:28:06 +0200

    Merging local work with the notebook on colab.. Just a checkpoint in case of messing up the project.

--- production/use.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-img = cv2.imread('Subject_0_0.png', cv2.IMREAD_GRAYSCALE) # 0
-cv2.imshow('Window Title', cv2.resize(img, (300, 200)))
-cv2.waitKey(0) # time to wait before closing in milliseconds (0 for infinite) 
